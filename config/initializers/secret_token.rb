2717149ca885ae89db2e30ac0e363ccaba805ba7
Carlos Urbín <carlos@urbin.es>
2011-01-18 13:39:01 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RepCan::Application.config.secret_token = 'a7a4dcffa51b65c055606f99486f8b42b1c3df7717905234d0ed9bae2ef40d6f71e996919044c0ee51c1dd459b577b4b298b7af36c0fcc0ae9cf732a608eb1eb'
