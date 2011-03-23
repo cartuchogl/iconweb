29031e5e4323266cdf6cbb89f8d31f9f37d34c08
Carlos Urbín <carlos@urbin.es>
2011-03-23 21:07:49 +0000

    The order is important

--- script/metameta.rb
+++ script/metameta.rb
@@ -23,77 +23,77 @@ parsers = Parsers.new
 
 metatable = [{
   #rails generate scaffold Unit 
-  'Unit' => {
-    :position => 'string',
-    :exp => 'integer',
-    :level => 'integer',
-    :name => 'string',
-    :name_unit => 'string',
-    :faction_name => 'string',
-    :cost => 'integer',
-    :move => 'integer',
-    :force => 'integer',
-    :resistance => 'integer',
-    :agility => 'integer',
-    :skill => 'integer',
-    :intelligence => 'integer',
-    :primary => '*bt(Weapon)',
-    :secondary => '*bt(Weapon)'
-  }},{
+  'Unit' => [
+    {:position => 'string'},
+    {:exp => 'integer'},
+    {:level => 'integer'},
+    {:name => 'string'},
+    {:name_unit => 'string'},
+    {:faction_name => 'string'},
+    {:cost => 'integer'},
+    {:move => 'integer'},
+    {:force => 'integer'},
+    {:resistance => 'integer'},
+    {:agility => 'integer'},
+    {:skill => 'integer'},
+    {:intelligence => 'integer'},
+    {:primary => '*bt(Weapon)'},
+    {:secondary => '*bt(Weapon)'}
+  ]},{
   
-  'Weapon' => {
-    :name => 'string',
-    :cost => 'integer',
-    :damage => 'string',
-    :clip => 'integer',
-    :total => 'integer',
-    :vshort => 'integer',
-    :vshort_bonus => 'integer',
-    :vshort_damage => 'string',
-    :short => 'integer',
-    :short_bonus => 'integer',
-    :short_damage => 'string',
-    :long => 'integer',
-    :long_bonus => 'integer',
-    :long_damage => 'string',
-    :vlong => 'integer',
-    :vlong_bonus => 'integer',
-    :vlong_damage => 'string'
-  }},{
+  'Weapon' => [
+    {:name => 'string'},
+    {:cost => 'integer'},
+    {:damage => 'string'},
+    {:clip => 'integer'},
+    {:total => 'integer'},
+    {:vshort => 'integer'},
+    {:vshort_bonus => 'integer'},
+    {:vshort_damage => 'string'},
+    {:short => 'integer'},
+    {:short_bonus => 'integer'},
+    {:short_damage => 'string'},
+    {:long => 'integer'},
+    {:long_bonus => 'integer'},
+    {:long_damage => 'string'},
+    {:vlong => 'integer'},
+    {:vlong_bonus => 'integer'},
+    {:vlong_damage => 'string'}
+  ]},{
   
-  'Squadron' => {
-    :name => 'string',
-    :player => '*bt(User)'
-  }},{
+  'Squadron' => [
+    {:name => 'string'},
+    {:player => '*bt(User)'}
+  ]},{
   
-  'Game' => {
-    :squadron1 => '*bt(Squadron)',
-    :squadron2 => '*bt(Squadron)',
-    :turns => '*hm(Turn)'
-  }},{
+  'Game' => [
+    {:squadron1 => '*bt(Squadron)'},
+    {:squadron2 => '*bt(Squadron)'},
+    {:turns => '*hm(Turn)'}
+  ]},{
   
-  'Turn' => {
-    :game => '*bt(Game)',
-    :moves => '*hm(Move)',
-    :fires => '*hm(Fire)'
-  }},{
+  'Turn' => [
+    {:game => '*bt(Game)'},
+    {:moves => '*hm(Move)'},
+    {:fires => '*hm(Fire)'}
+  ]},{
   
-  'Move' => {
-    :unit => '*bt(Unit)',
-    :x => 'float',
-    :y => 'float',
-    :turn => '*bt(Turn)'
-  }},{
+  'Move' => [
+    {:unit => '*bt(Unit)'},
+    {:x => 'float'},
+    {:y => 'float'},
+    {:turn => '*bt(Turn)'}
+  ]},{
   
-  'Fire' => {
-    :orig => '*bt(Unit)',
-    :dest => '*bt(Unit)',
-    :distance => 'integer',
-    :dice => 'integer',
-    :coverage => 'float',
-    :turn => '*bt(Turn)',
-    :damage => 'integer'
-  }
+  'Fire' => [
+    {:orig => '*bt(Unit)'},
+    {:dest => '*bt(Unit)'},
+    {:distance => 'integer'},
+    {:dice => 'integer'},
+    {:coverage => 'float'},
+    {:turn => '*bt(Turn)'},
+    {:damage => 'integer'}
+  ]
 }]
 
 metametanames = []
@@ -102,7 +102,7 @@ metatable.each do |item|
   name = item.keys.first
   cmd = "rails g scaffold #{name} "
   metametanames << "#{name}"
-  item[item.keys.first].each { |k,v| cmd += parsers.process(k,v).to_s+" " }
+  item[item.keys.first].each { |prop| cmd += parsers.process(prop.keys.first,prop[prop.keys.first]).to_s+" " }
   puts cmd
   `#{cmd}`
 end
